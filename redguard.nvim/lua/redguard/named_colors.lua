ed2ca528917ae035e9035207112338a32f77f805
Ellis Coon <ellis.coon@equusoft.com>
2025-08-23 21:59:20 -0600

    refined the edge theme a bit more

--- redguard.nvim/lua/redguard/named_colors.lua
+++ redguard.nvim/lua/redguard/named_colors.lua
@@ -1,24 +1,24 @@
 local redguard = {
 	--16 colors
-	black = "#2E2A2C",
-	dark_gray = "#3B2F34",
-	gray = "#44353C",
-	light_gray = "#4D3C45",
-	light_gray_bright = "#66515B",
-	darkest_white = "#D8DEE9",
-	darker_white = "#E5E9F0",
-	white = "#ECEFF4",
-	rose = "#D08CA3",
-	pink = "#E38BA1",
-	crimson = "#C46C7F",
-	wine = "#A24C5E",
-	red = "#BF616A",
-	orange = "#D08770",
-	yellow = "#EBCB8B",
-	blue = "#5e81ac",
-	dark_blue = "#4c566a",
-	green = "#A3BE8C",
-	purple = "#B48EAD",
+	black = "#2E2A2C", --[46, 42, 44]
+	dark_gray = "#3B2F34", --[59, 47, 52]
+	gray = "#44353C", --[68, 53, 60]
+	light_gray = "#4D3C45", --[77, 60, 69]
+	light_gray_bright = "#66515B", --[102, 81, 91]
+	darkest_white = "#D8DEE9", --[216, 222, 233]
+	darker_white = "#E5E9F0", --[229, 233, 240]
+	white = "#ECEFF4", --[236, 239, 244]
+	rose = "#D08CA3", --[208, 140, 163]
+	pink = "#E38BA1", --[227, 139, 161]
+	crimson = "#C46C7F", --[196, 108, 127]
+	wine = "#A24C5E", --[162, 76, 94]
+	red = "#BF616A", --[191, 97, 106]
+	orange = "#D08770", --[208, 135, 112]
+	yellow = "#EBCB8B", --[235, 203, 139]
+	blue = "#5e81ac", --[94, 129, 172]
+	dark_blue = "#4c566a", --[76, 86, 106]
+	green = "#A3BE8C", --[163, 190, 140]
+	purple = "#B48EAD", --[180, 142, 173]
 	none = "NONE",
 }
 return redguard
